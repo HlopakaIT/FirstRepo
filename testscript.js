d463c15fe25490ef0be02d777d031b2f785c48de
HlopakA <faka2@ukr.net>
2024-10-29 05:22:24 +0300

    ver 0.13

---
+++ testscript.js
@@ -0,0 +1,12 @@
+const lyrics = document.getElementById("dom");
+console.log(lyrics);
+const namee = document.getElementsByClassName("name");
+console.log(namee);
+const songss = document.getElementsByTagName("a");
+console.log(songss);
+const dommm = document.querySelector("#dom");
+console.log(dommm);
+const list = document.querySelectorAll("body");
+console.log(list);
+const links = document.links[0]
+console.log(links);
